d6094e21af1423a42b90ac8c97c727c65b64a347
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-05-18 11:51:41 +0200

    installed prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		es6: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "prettier"],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	rules: {},
+}
