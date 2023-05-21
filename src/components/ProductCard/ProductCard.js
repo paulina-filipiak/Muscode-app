6ef867c944b6de08bb6918eeebc5cee29f97dded
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-05-21 23:14:53 +0200

    added ribbons with discount

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,6 +1,12 @@
 export const ProductCard = (product) => {
+	const discount = ((product.price - product.promoPrice) / product.price) * 100
 	return /* HTML */ `
 		<section class="card container" id="${product?.id}">
+			<div
+				style="display:${product.promoPrice ? "block" : "none"}"
+				class="ribbon">
+				<p class="ribbon-price">-${discount.toFixed(0)}%</p>
+			</div>
 			<h3 class="card__name">${product?.name}</h3>
 			<img
 				class="card__img"
